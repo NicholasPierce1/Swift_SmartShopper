f978c0638f66550c0930b4c00c250f36fb8b2d89
Nick Pierce <s529822@nwmissouri.edu>
2019-02-24 22:54:50 -0600

    Instigated the XCode Project and rendered the basic Views with their containing ViewControllers with apt names.

---
+++ SmartShopper/SmartShopper/AdminFeaturesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AdminFeaturesViewController.swift
+//  SmartShopper
+//
+//  Created by Nick Pierce on 2/24/19.
+//  Copyright © 2019 SmartShopperTeam. All rights reserved.
+//
+
+import UIKit
+
+class AdminFeaturesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ SmartShopper/SmartShopper/SearchItemViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SmartShopper
+//
+//  Created by Nick Pierce on 2/24/19.
+//  Copyright © 2019 SmartShopperTeam. All rights reserved.
+//
+
+import UIKit
+
+class SearchItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

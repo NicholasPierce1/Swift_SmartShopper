cd9908d4921cc83a388e3161a46a77e569d03fac
Nick Pierce <s529822@nwmissouri.edu>
2019-02-25 15:25:36 -0600

    Other Amenities added. Still more to come.

--- SmartShopper/SmartShopper/WelcomeScreenViewController.swift
+++ SmartShopper/SmartShopper/WelcomeScreenViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class WelcomeScreenViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func loadView() {
+        super.loadView()
+        
+        let layer = CAGradientLayer()
+        layer.frame = view.bounds
+        layer.colors = [UIColor.lightGray, UIColor.darkGray]
+        layer.startPoint = CGPoint(x: 0, y: 0)
+        layer.endPoint = CGPoint(x: 1, y: 1)
+        
+        view.layer.addSublayer(layer)
+        
+    }
 
 }
 

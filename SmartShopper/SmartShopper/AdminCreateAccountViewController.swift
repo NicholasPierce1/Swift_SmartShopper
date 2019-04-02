a8e366045689e78884c9b4b2b95dbbda7eb67bc8
Satyakanth <satyakanthk440@gmail.com>
2019-04-02 18:22:22 -0500

    fixed some issues with signup

--- SmartShopper/SmartShopper/AdminCreateAccountViewController.swift
+++ SmartShopper/SmartShopper/AdminCreateAccountViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class AdminCreateAccountViewController: UIViewController {
-
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var cpassTF: UITextField!
+    @IBOutlet weak var storePasswordTF: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    
+    func display(title:String, msg:String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func signupBtn(_ sender: Any) {
+        
+        if let userName = userNameTF.text, let password = passwordTF.text, let cpass = cpassTF.text, let sPassword = Int(storePasswordTF.text!) {
+            if password == cpass {
+                let admin =  Store.shared.addAdmin(withUsername: userName, adminPassword: password, forStore: sPassword, storePassword: sPassword)
+                print(admin)
+                if admin ==  ReturnCode.adminAdded {
+                    self.display(title: "Admin Created", msg: "Go back to login screen")
+                } else if admin == ReturnCode.adminAlreadyExist {
+                    self.display(title: "Admin Already Exists", msg: "Please try diffrent credentials")
+                } else {
+                    self.display(title: "Error", msg: "Please try diffrent credentials")
+                }
+            } else {
+                self.display(title: "Password doesn't match", msg: "Please try diffrent credentials")
+            }
+            
+        }
+        
     }
     
 
--- SmartShopper/SmartShopper/AdminLoginViewController.swift
+++ SmartShopper/SmartShopper/AdminLoginViewController.swift
@@ -10,19 +10,46 @@ import UIKit
 
 class AdminLoginViewController: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var spassTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        if identifier == "login" {
-            return  true// Store.shared.
-        }
-        
-        return false
+    @IBAction func unwindToLogin(_ unwindSegue: UIStoryboardSegue) {}
+    
+    func display(title:String, msg:String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
+    
+    @IBAction func signupBtn(_ sender: Any) {
+        self.performSegue(withIdentifier: "createAdmin", sender: self)
+    }
+    @IBAction func loginBtn(_ sender: Any) {
+        if let userName = userNameTF.text, let password = passwordTF.text, let spass = Int(spassTF.text!) {
+            
+            
+            let login =  Store.shared.login(username: userName, adminPassword: password, storePassword: spass)
+            if login {
+                self.performSegue(withIdentifier: "login", sender: self)
+            } else {
+                self.display(title: "Login Failed", msg: "Please check credentials")
+            }
+        }
+    }
+//    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+//        if identifier == "login" {
+//            return  true// Store.shared.
+//        }
+//
+//        return false
+//    }
     /*
     // MARK: - Navigation
 

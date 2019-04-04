12221008e7defa0f931aa96a0f6aa088d05c0031
satyakanth <satyakanthk440@gmail.com>
2019-04-04 17:32:33 -0500

    Re-added navigation bar for signup, updated according to nick's comments. Added extra alerts for text fields.

--- SmartShopper/SmartShopper/AdminCreateAccountViewController.swift
+++ SmartShopper/SmartShopper/AdminCreateAccountViewController.swift
@@ -10,13 +10,62 @@ import UIKit
 
 class AdminCreateAccountViewController: UIViewController {
 
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var confirmPasswordTF: UITextField!
+    @IBOutlet weak var storeNumberTF: UITextField!
+    @IBOutlet weak var storePasswordTF: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    override func viewWillAppear(_ animated: Bool) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+        confirmPasswordTF.text = ""
+        storeNumberTF.text = ""
+        storePasswordTF.text = ""
+    }
+    func display(title:String, msg:String, success:Bool) {
+    let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+    var action:UIAlertAction
+    if success {
+    action = UIAlertAction(title: "OK", style: .default, handler: { _ in
+    self.dismiss(animated: true, completion: nil)
+    })
+    } else {
+    action = UIAlertAction(title: "OK", style: .default, handler: nil)
+    }
     
-
+    alert.addAction(action)
+    present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func backBtn(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func signUpBtn(_ sender: Any) {
+        if let userName = userNameTF.text, let password = passwordTF.text, let confirmPassword = confirmPasswordTF.text, let sNumber = Int(storeNumberTF.text!), let sPassword = Int(storePasswordTF.text!) {
+            if password == confirmPassword { // checking for matching password
+                let admin =  Store.shared.addAdmin(withUsername: userName, adminPassword: password, forStore: sNumber, storePassword: sPassword)
+                print(admin)
+                if admin ==  ReturnCode.adminAdded {
+                    self.display(title: "Admin Created", msg: "Go back to login screen", success: true)
+                } else if admin == ReturnCode.adminAlreadyExist {
+                    self.display(title: "Admin Already Exists", msg: "Please try diffrent credentials", success: false)
+                } else {
+                    self.display(title: "Error", msg: "Please try diffrent credentials" , success: false)
+                }
+            } else {
+                self.display(title: "Password doesn't match", msg: "Please try diffrent credentials" , success: false)
+            }
+            
+        } else {
+            self.display(title: "Error", msg: "Please enter all required fields" , success: false)
+        }
+    }
     /*
     // MARK: - Navigation
 
--- SmartShopper/SmartShopper/AdminLoginViewController.swift
+++ SmartShopper/SmartShopper/AdminLoginViewController.swift
@@ -16,12 +16,47 @@ class AdminLoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var storePasswordTF: UITextField!
+    
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         if identifier == "login" {
             return  true// Store.shared.
         }
         
         return false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        userNameTF.text = ""
+        passwordTF.text = ""
+        storePasswordTF.text = ""
+    }
+    
+    func display(title:String, msg:String) {
+        let alert = UIAlertController(title: title, message: msg, preferredStyle: .alert)
+        let action = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func signUpBtn(_ sender: Any) {
+        self.performSegue(withIdentifier: "createAdmin", sender: self)
+    }
+    @IBAction func loginBtn(_ sender: Any) {
+        if let userName = userNameTF.text, let password = passwordTF.text, let spass = Int(storePasswordTF.text!) {
+            
+            
+            let login =  Store.shared.login(username: userName, adminPassword: password, storePassword: spass)
+            if login {
+                self.performSegue(withIdentifier: "login", sender: self)
+            } else {
+                self.display(title: "Login Failed", msg: "Please check credentials")
+            }
+        } else {
+            self.display(title: "Error", msg: "Please enter all required fields")
+        }
     }
     /*
     // MARK: - Navigation
